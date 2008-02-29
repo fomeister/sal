40d74255621f383090c27e4ba41240844927d1f5
jc138789 <jc138789@928199ce-fcca-11dd-ac65-f74f249cfaac>
2008-02-29 07:38:15 +0000

    OWFS almost there

--- SAL/trunk/run-sg1.sh
+++ SAL/trunk/run-sg1.sh
@@ -1,1 +1,2 @@
-java -classpath ./lib/log4j-1.2.15.jar:./bin jcu.sal.Agent.SALAgent "src/pc-osdata-sg1.xml" "src/sensors.xml"
+#java -classpath ./lib/log4j-1.2.15.jar:./bin jcu.sal.Agent.SALAgent "src/pc-osdata-sg1.xml" "src/sensors.xml"
+java -classpath ./lib/log4j-1.2.15.jar:./bin jcu.sal.Agent.SALAgent "src/platformConfig-owfs.xml" "src/sensors-owfs-sg1.xml"
