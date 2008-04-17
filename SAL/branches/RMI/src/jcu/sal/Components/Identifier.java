2c021ad3d4b0f4984056d83a7c525a5e174e7f8d
jc138789 <jc138789@928199ce-fcca-11dd-ac65-f74f249cfaac>
2008-04-17 06:43:55 +0000

    Branched SAL to RMI

---
+++ SAL/branches/RMI/src/jcu/sal/Components/Identifier.java
@@ -0,0 +1,29 @@
+package jcu.sal.Components;
+
+public interface Identifier {
+	
+	/**
+	 * Returns the name of a component from its identifier
+	 * @return a string representation of the identifier
+	 */
+	public String getName();
+	
+	/**
+	 * Test whether two identifiers are the same
+	 * @param id the identifier to be tested
+	 * @return ture or false
+	 */
+	public boolean equals(Object id);
+	
+	/**
+	 * Returns a hash value of this object
+	 * @return the has code
+	 */
+	public int hashCode();
+	
+	/**
+	 * returns a string representation of the identifier
+	 * @return the string representation of the identifier
+	 */
+	public String toString();
+}
