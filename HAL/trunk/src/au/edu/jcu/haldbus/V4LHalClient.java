600961851cf2be6935226ac3a50ba63c4ca6253e
jc138789 <jc138789@928199ce-fcca-11dd-ac65-f74f249cfaac>
2008-05-27 05:28:44 +0000

    HAL commit: Added initial HAL device check to autodetect already-connected devices

--- HAL/trunk/src/au/edu/jcu/haldbus/V4LHalClient.java
+++ HAL/trunk/src/au/edu/jcu/haldbus/V4LHalClient.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import au.edu.jcu.haldbus.exceptions.AddRemoveElemException;
 import au.edu.jcu.haldbus.exceptions.InvalidConstructorArgs;
 import au.edu.jcu.haldbus.match.AlwaysMatch;
+import au.edu.jcu.haldbus.match.NextMatch;
 import au.edu.jcu.haldbus.match.TypeMatch;
 import au.edu.jcu.haldbus.match.VectorMatch;
 
@@ -14,18 +15,20 @@ public class V4LHalClient extends AbstractDeviceDetection {
 	
 	
 	public V4LHalClient() throws InvalidConstructorArgs, AddRemoveElemException{
-		super();
+		super(SUBSEQUENT_RUN_FLAG);
 		addMatch("capability", new VectorMatch<String>("info.capabilities", "video4linux"));
 		addMatch("category", new TypeMatch<String>("info.category", "video4linux"));
 		addMatch("category.capture", new VectorMatch<String>("info.capabilities", "video4linux.video_capture"));
 		addMatch("deviceFile", new AlwaysMatch("linux.device_file"));
+		addMatch("info.product", new NextMatch("@info.parent", new AlwaysMatch("info.product")));
+		addMatch("info.vendor", new NextMatch("@info.parent", new AlwaysMatch("info.vendor")));
 		//addMatch("usbVendor", new NextMatch("@info.parent", new AlwaysMatch("usb_device.vendor_id"));
 		//addMatch("usbProduct", new NextMatch("@info.parent", new AlwaysMatch("usb_device.product_id"));		
 	}
 
 	@Override
 	public void doAction(Map<String,String> l) {
-		System.out.println("Doing action");
+		System.out.println("Found "+l.get("info.product")+" - "+l.get("info.vendor")+ " on "+l.get("deviceFile"));
 	}
 
 	@Override
--- HAL/trunk/src/au/edu/jcu/haldbus/match/AbstractMatch.java
+++ HAL/trunk/src/au/edu/jcu/haldbus/match/AbstractMatch.java
@@ -4,8 +4,43 @@ import au.edu.jcu.haldbus.exceptions.InvalidConstructorArgs;
 import au.edu.jcu.haldbus.exceptions.InvalidMethodCall;
 import au.edu.jcu.haldbus.exceptions.MatchNotFoundException;
 
+/**
+ * This class impements the barebones structure of a Hal Match object. It mostly handles contrstuction of the object
+ * and makes sure the right attributes are initialised. It then declares somme get() methods. Subclasses need only
+ * implement a proper constructor and a matchObject() method.<br>HAL match object have some form of recursivity: they can
+ * hold a reference to another HAL match object (referred to as the <b>"next HAL match object"</b>). Two scenarios can happen:<ul>
+ * <li>The simplest one: an HAL match object is to be matched against a property found in the current HAL object. In this
+ * case, the match object contains the name of the property to be checked, and the caller invokes the <code>match()</code>
+ * method with the value of the property as the sole argument.<li>
+ * </li>The more complicated one: an HAL match object is to be matched against a property found in another HAL object
+ * (referred to as the <b>"next HAL object"</b>), and the UDI of the "next HAL object" is either known in advance (and stored in
+ * this HAL match) or can be found in the value of a property in the current HAL object. In this case, the caller invokes
+ * either <code>getNextObjectValue()</code> or <code>getNextObjectLink()</code> in order to find the next HAL object's UDI.
+ * Once the properties of the next HAL object have been retrieved, they will be matched against the next HAL match object, whose
+ * reference is found in this HAL match object (recursivity)</li></ul>
+ * Classes using HalMatchInterface objects to find a match follow this example:<br>
+ * <code>public String findMatch(HalMatchInterface m, Map&lt;String,Object&gt; props)</code> 
+ * <ul><li>if <code>m.matchThisObject()</code> then
+ * 	<ul><li>Get the name of the property of interest: <code>p = m.getPropName()</code></li>
+ * 	<li><code>return match(props.get(p))</code></li></ul>
+ * <li>else if <code>m.matchNextObjectValue()</code> then
+ * 	<ul><li>Get the name of the next HAL object: <code>m.getNextObjectValue()</code></li>
+ * 	<li>Get its list of properties: <code>nextProps</code></li>
+ *  <li>Get the next HAL match object: <code>nextMatch = getNextMatch()</code></li>
+ *  <li><code>return findMatch(nextMatch, nextProps)</code>
+ * 	</ul>
+ * <li>else if <code>m.matchNextObjectLink()</code> then
+ * 	<ul><li>Get the link to the next HAL object: <code>m.getNextObjectLink()</code></li>
+ * 	<li>Get its name and list of properties <code>nextProps</code></li>
+ *  <li>Get the next HAL match object: <code>nextMatch = getNextMatch()</code></li>
+ *  <li><code>return findMatch(nextMatch, nextProps)</code>
+ * 	</ul>
+ * </ul>
+ * @author gilles
+ *
+ */
 public abstract class AbstractMatch implements HalMatchInterface {
-	public final static String THIS_OBJECT = "#ShouldbeFine@@#";
+	public final static String THIS_OBJECT = "`#ShouldbeFine@@#";
 	private String name;
 	private String object;
 	private String propName;
@@ -33,62 +68,147 @@ public abstract class AbstractMatch implements HalMatchInterface {
 			throw new InvalidConstructorArgs();
 		}
 	}
-
+	
+	/**
+	 * This constructor builds a HAL Match object against a property found in another HAL object, not the current one.
+	 * This HAL object is referred to either by its name or by a link pointing to its name:
+	 * <ul><li>In the former case, the <code>obj</code> argument contains the name (UDI) of the next HAL object to be
+	 * checked.</li>
+	 * <li>In the latter case, the link is simply the name of a property found preceeded by a '@' sign in the
+	 * current HAL object that contains the name of the next HAL object object to be checked.</li></ul>
+	 * In both cases, when the next HAL object's properties have been retrieved, they will be matched against the
+	 * given <code>HalMatchInterface n</code> instance.  
+	 * @param obj either the UDI of the next HAL object to be checked, or a link to it ('@' followed by the name
+	 * of a property found in the current HAL object which contains the name of the next HAL object).
+	 * @param n the HalMatchInterface object against which the next HAL object will be matched 
+	 * @param name the name of this Hal match
+	 * @throws InvalidConstructorArgs if either of the arguments have invalid values
+	 */
 	protected AbstractMatch(String obj, HalMatchInterface n, String name) throws InvalidConstructorArgs {
 		this(obj, null, n, name);
 	}
 
+	/**
+	 * This constructor builds an HAL match object against a property in the current HAL object. The property to be checked
+	 * is referred to by its name (<code>prop</code> argument). 
+	 * @param prop the name of the property to be checked 
+	 * @param name the name of the HAL match object
+	 * @throws InvalidConstructorArgs if either of the arguments have invalid values
+	 */
 	protected AbstractMatch(String prop, String name) throws InvalidConstructorArgs {
 		this(null, prop, null, name);
 	}
 
-	public String match(Object o) throws MatchNotFoundException {
+	/**
+	 * This method is called when the a property in the current HAL object is to be matched. The value of this property
+	 * is the sole argument. If the property has the right value, the method returns a string representation of the object o.
+	 * Otherwise the methods throws a MatchNotFound exception.
+	 * @param o the value of the property
+	 * @return a string representation of the object o
+	 * @throws MatchNotFoundException if the property doesnt have the expected value.
+	 */
+	public final String match(Object o) throws MatchNotFoundException {
 		if (o == null)
 			throw new MatchNotFoundException();
 
 		return matchObject(o);
 	}
 
+	/**
+	 * This method must be implemented by subclasses. It is up to the subclasses to decide when a match is found, based on 
+	 * the value of the property. If the match is found, this method must return a string (which really should be 
+	 * <code>o.toString()</code> or similar)
+	 * @param o the value of the property.
+	 * @return the string representation of o 
+	 * @throws MatchNotFoundException if the property doesnt have the expected value.
+	 */
 	protected abstract String matchObject(Object o)	throws MatchNotFoundException;
 
-
-	public boolean matchThisObject() {
+	/**
+	 * This method allows the caller to determine whether this Hal match is against a property found in the current
+	 * HAL object.
+	 * @return whether this Hal match is against a property found in the current HAL object.
+	 */
+	public final boolean matchThisObject() {
 		return object.equals(THIS_OBJECT);
 	}
 
-	public boolean matchNextObjectLink() {
+	/**
+	 * This method allows the caller to determine whether this Hal match is against a property found in another
+	 * HAL object, referred to by a link.
+	 * @return whether this Hal match is against a property found in another HAL object, referred to by a link.
+	 */
+	public final boolean matchNextObjectLink() {
 		return object.charAt(0) == '@';
 	}
 
-	public boolean matchNextObjectValue() {
+	/**
+	 * This method allows the caller to determine whether this Hal match is against a property found in another
+	 * HAL object, referred to by its UDI.
+	 * @return whether this Hal match is against a property found in another HAL object, referred to by its UDI.
+	 */
+	public final boolean matchNextObjectValue() {
 		return object.charAt(0) == '/';
 	}
 	
-	public HalMatchInterface getNextMatch() throws  InvalidMethodCall{
+	/**
+	 * In the case where this HAL match object is against a property found in another HAL object, this method will
+	 * return the HAL match object or throw an exception if this HAL match object is against the current HAL object.
+	 * @return the next HAL match object
+	 * @throws InvalidMethodCall if this HAL match object is against the current HAL object (there is no "next
+	 * HAL match object").
+	 */
+	public final HalMatchInterface getNextMatch() throws  InvalidMethodCall{
 		if(matchNextObjectLink() || matchNextObjectValue())
 			return nextMatch;
 		throw new InvalidMethodCall();
 	}
 
-	public String getNextObjectLink() throws InvalidMethodCall {
+	/**
+	 * In the case where this HAL match object is against a property found in another HAL object referred to by a link, 
+	 * this method will return the name of the property (in the current HAL object) where the UDI of the next HAL object
+	 * is found. This method throws an exception if this HAL match object is against the current HAL object or another
+	 * HAL object referred to using its name.
+	 * @return the name of the property (in the current HAL object) where the UDI of the next HAL object is found.
+	 * @throws InvalidMethodCall if this HAL match object is against the current HAL object (there is no "next
+	 * HAL match object"), or the next HAL object is referred to using its name (not a link).
+	 */
+	public final String getNextObjectLink() throws InvalidMethodCall {
 		if (matchNextObjectLink())
 			return object.substring(1);
 		throw new InvalidMethodCall();
 	}
 
-	public String getNextObjectValue() throws InvalidMethodCall {
+	/**
+	 * In the case where this HAL match object is against a property found in another HAL object referred to by its UDI,
+	 * This method will return the UDI of the next HAL object. This method throws an exception if this HAL match object
+	 * is against the current HAL object or another HAL object referred to using a link.
+	 * @return the UDI of the next HAL object.
+	 * @throws InvalidMethodCall if this HAL match object is against the current HAL object (there is no "next HAL match
+	 * object"), or the next HAL object is referred to using a link (not its UDI).
+	 */
+	public final String getNextObjectValue() throws InvalidMethodCall {
 		if (matchNextObjectValue())
 			return object;
 		throw new InvalidMethodCall();
 	}
 
-	public String getPropName() throws InvalidMethodCall {
+	/**
+	 * In the case where this HAL match object is against the current HAL object,this method will return the name of the
+	 * property to be matched. This method throws an exception if this HAL match object is against another HAL object.
+	 * @return the name of the property to be matched.
+	 * @throws InvalidMethodCall if this HAL match object is against against another HAL object.
+	 */
+	public final String getPropName() throws InvalidMethodCall {
 		if (matchThisObject())
 			return propName;
 		throw new InvalidMethodCall();
 	}
 
-	public String getName() {
+	/**
+	 * This method returns the name of this HAl match object.
+	 */
+	public final String getName() {
 		return name;
 	}
 }
