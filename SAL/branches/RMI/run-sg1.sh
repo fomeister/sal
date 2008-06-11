c1e3cda2079afe31e4e8088b2ca919fe5f01ff6f
jc138789 <jc138789@928199ce-fcca-11dd-ac65-f74f249cfaac>
2008-06-11 05:19:57 +0000

    Removed old SAL RMI Branch

--- SAL/branches/RMI/run-sg1.sh
+++
@@ -1,2 +0,0 @@
-#java -classpath ./lib/log4j-1.2.15.jar:./bin jcu.sal.Agent.SALAgent "src/pc-osdata-sg1.xml" "src/sensors.xml"
-java -classpath ./lib/*:./bin jcu.sal.SALuser "src/platformConfig-owfs.xml" "src/sensors-owfs-hb.xml"
