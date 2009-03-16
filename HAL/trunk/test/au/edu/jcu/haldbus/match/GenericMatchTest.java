a21e9f49ef19779be5059598751b050398046242
gilles.gigan <gilles.gigan@928199ce-fcca-11dd-ac65-f74f249cfaac>
2009-03-16 11:51:50 +0000

    HAL trunk:   * Updated README file   * fixed JUNIT tests

--- HAL/trunk/test/au/edu/jcu/haldbus/match/GenericMatchTest.java
+++ HAL/trunk/test/au/edu/jcu/haldbus/match/GenericMatchTest.java
@@ -4,36 +4,36 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-import au.edu.jcu.haldbus.exceptions.InvalidConstructorArgs;
+import au.edu.jcu.haldbus.exceptions.InvalidArgumentsException;
 import au.edu.jcu.haldbus.exceptions.MatchNotFoundException;
 
 public class GenericMatchTest {
 
-	@Test(expected=InvalidConstructorArgs.class)
-	public void testMatchObject() throws InvalidConstructorArgs {
+	@Test(expected=InvalidArgumentsException.class)
+	public void testMatchObject() throws InvalidArgumentsException {
 		new GenericMatch<Integer>("name", new Integer(12), true, true);
 	}
 	
 	@Test
-	public void testMatchObject2() throws InvalidConstructorArgs, MatchNotFoundException {
+	public void testMatchObject2() throws InvalidArgumentsException, MatchNotFoundException {
 		GenericMatch<Integer> m = new GenericMatch<Integer>("name", new Integer(12));
 		assert(m.match((Object) new Integer(12)).equals("12"));
 	}
 	
 	@Test
-	public void testMatchObject3() throws InvalidConstructorArgs, MatchNotFoundException {
+	public void testMatchObject3() throws InvalidArgumentsException, MatchNotFoundException {
 		GenericMatch<Integer> m = new GenericMatch<Integer>("name", new Integer(12));
 		assert(m.match((Object) new Integer(12)).equals("1"));
 	}
 	
 	@Test(expected=MatchNotFoundException.class)
-	public void testMatchObject4() throws InvalidConstructorArgs, MatchNotFoundException {
+	public void testMatchObject4() throws InvalidArgumentsException, MatchNotFoundException {
 		GenericMatch<Integer> m = new GenericMatch<Integer>("name", new Integer(12));
 		m.match((Object) new Integer(1));
 	}	
 
 	@Test
-	public void testGenericMatchStringT() throws InvalidConstructorArgs, MatchNotFoundException {
+	public void testGenericMatchStringT() throws InvalidArgumentsException, MatchNotFoundException {
 		GenericMatch<String> m = new GenericMatch<String>("name", "hello");
 		assert(m.match((Object) "hello").equals("hello"));
 		try {
@@ -50,7 +50,7 @@ public class GenericMatchTest {
 	}
 
 	@Test
-	public void testGenericMatchStringTBooleanBoolean() throws InvalidConstructorArgs, MatchNotFoundException {
+	public void testGenericMatchStringTBooleanBoolean() throws InvalidArgumentsException, MatchNotFoundException {
 		GenericMatch<String> m = new GenericMatch<String>("name", "hello", true, true);
 		m.match((Object) "hello");
 		m.match((Object) "BonjourHellObjectDamnit");
@@ -59,7 +59,7 @@ public class GenericMatchTest {
 	}
 	
 	@Test
-	public void testGenericMatchStringTBooleanBoolea2n() throws InvalidConstructorArgs, MatchNotFoundException {
+	public void testGenericMatchStringTBooleanBoolea2n() throws InvalidArgumentsException, MatchNotFoundException {
 		GenericMatch<String> m = new GenericMatch<String>("name", "hello", true, false);
 		m.match((Object) "hello");
 		m.match((Object) "BonjourhellobjectDamnit");
@@ -68,7 +68,7 @@ public class GenericMatchTest {
 	}
 	
 	@Test(expected=MatchNotFoundException.class)
-	public void testGenericMatchStringTBooleanBoolean3() throws InvalidConstructorArgs, MatchNotFoundException {
+	public void testGenericMatchStringTBooleanBoolean3() throws InvalidArgumentsException, MatchNotFoundException {
 		GenericMatch<String> m = new GenericMatch<String>("name", "hello", false, true);
 		try {
 			m.match((Object) "hello");
